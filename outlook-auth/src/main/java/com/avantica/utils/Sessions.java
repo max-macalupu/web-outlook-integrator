fcf232f79443f1d43c825034c2ea72ff00f036bb
max.macalupu <maxleoj@gmail.com>
2016-06-01 18:33:47 -0500

    agregando clase que manejara sessiones en un simple HashMap

---
+++ outlook-auth/src/main/java/com/avantica/utils/Sessions.java
@@ -0,0 +1,32 @@
+package com.avantica.utils;
+
+import java.util.HashMap;
+
+import com.avantica.web.model.BodyToken;
+
+public class Sessions extends HashMap<String, BodyToken>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static Sessions sessions;
+	
+	private Sessions(){
+	}
+	
+	private static Sessions getInstance(){
+		if( sessions == null){
+			sessions = new Sessions();
+		}
+		return sessions;
+	}
+
+	public synchronized static void nuevo(BodyToken info){
+		getInstance().put(info.getOid(), info);
+	}
+	
+	public static BodyToken getSession(String key){
+		return getInstance().get(key);
+	}
+}
