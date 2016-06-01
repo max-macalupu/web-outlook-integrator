b1766f2ee26ec302a985ac244c8031913070f296
max.macalupu <maxleoj@gmail.com>
2016-06-01 18:02:06 -0500

    Modificacion en redireccionamiento del loggin

--- outlook-auth/src/main/java/com/avantica/web/controller/RedirectAuth.java
+++ outlook-auth/src/main/java/com/avantica/web/controller/RedirectAuth.java
@@ -1,11 +1,11 @@
 package com.avantica.web.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.avantica.utils.QueryString;
+import com.avantica.web.model.OutlookParameters;
 
 @Controller
 public class RedirectAuth{
@@ -15,10 +15,15 @@ public class RedirectAuth{
 	 */
 	
 	@RequestMapping(value = "/redirectAuth", method = RequestMethod.GET)
-	public String printWelcome(HttpServletRequest request,
-			HttpServletResponse response) {
-		String redirectUrl = "http://www.google.com";
-	     return "redirect:" + redirectUrl;
+	public String printWelcome() {
+		String completeURL =  QueryString.withDomain(OutlookParameters.TOKEN_LOGIN.getCodigo())
+										 .andParameter(OutlookParameters.CLIEND_ID)
+										 .andParameter(OutlookParameters.REDIRECT)
+										 .andParameter(OutlookParameters.AUTHORIZ)
+										 .andParameter(OutlookParameters.SCOPE)
+										 .andParameter(OutlookParameters.RESPONSE_TYPE_TOKEN)
+										 .giveMeURL();
+	     return "redirect:" + completeURL;
 	}
 
 }
