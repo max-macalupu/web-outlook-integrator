c36ea49f09f7497e51d67b92c7552ddf1c33a88b
max.macalupu <maxleoj@gmail.com>
2016-06-02 10:54:02 -0500

    adding parameters in change

--- outlook-auth/src/main/java/com/avantica/web/model/OutlookParameters.java
+++ outlook-auth/src/main/java/com/avantica/web/model/OutlookParameters.java
@@ -5,11 +5,13 @@ public enum OutlookParameters {
 	TOKEN_URL("url", "https://login.microsoftonline.com/common/oauth2/v2.0/token"),
 	TOKEN_LOGIN("url", "https://login.microsoftonline.com/common/oauth2/v2.0/authorize"),
 	CLIEND_ID("client_id", "bba8a36d-866a-4745-9bb3-018fd877587b"),
-	CLIEND_SEC("client_secret", "4RNhHLZU9cJffLFRraPp8fj"),
+	CLIEND_SEC("client_secret", "rOXah2VrXRknsdP3p3TYHJK"),
 	AUTHORIZ("grant_type", "authorization_code"),
 	RESPONSE_TYPE_CODE("response_type", "code"),
-	SCOPE("scope", "https://outlook.office.com/mail.read"),
-	REDIRECT("redirect_uri", "http://localhost:8080/outlook-auth/");
+	RESPONSE_TYPE_TOKEN("response_type", "token"),
+	SCOPE("scope", "openid+offline_access+profile+https://outlook.office.com/mail.read+https://outlook.office.com/mail.send"),
+	REDIRECT("redirect_uri", "https://xinefserver.com:9090/outlook-auth/autenticate"),
+	RESOURCE("resource", "https://xinefserver.com:9090/outlook-auth/");
 	
 	private String descripcion;
 	private String codigo;
