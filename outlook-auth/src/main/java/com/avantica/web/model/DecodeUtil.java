dca7a95d854bb5d3dc0d2ac8a38b1d12046bcf33
max.macalupu <maxleoj@gmail.com>
2016-06-02 11:09:41 -0500

    Nuevas clases para almacenar informacion de tokem

---
+++ outlook-auth/src/main/java/com/avantica/web/model/DecodeUtil.java
@@ -0,0 +1,18 @@
+package com.avantica.web.model;
+
+import org.apache.commons.codec.binary.Base64;
+
+import com.google.gson.Gson;
+
+public class DecodeUtil {
+	
+    private static final Base64 base64Url = new Base64();
+
+
+	public static BodyToken decodeToken(String token){
+		String[] jwtTokenValues = token.split("\\.");
+        String value = new String(base64Url.decode(jwtTokenValues[1].getBytes()));
+        Gson gsonObject = new Gson();
+		return gsonObject.fromJson(value,BodyToken.class);
+	}
+}
