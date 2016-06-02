5a2af1b700e9afb869e7d79d338c9e579ff88e09
max.macalupu <maxleoj@gmail.com>
2016-06-02 10:33:52 -0500

    session controller

---
+++ outlook-auth/src/main/java/com/avantica/web/controller/SessionController.java
@@ -0,0 +1,28 @@
+package com.avantica.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.avantica.utils.Sessions;
+import com.avantica.web.model.BodyToken;
+import com.avantica.web.model.DecodeUtil;
+
+@Controller
+public class SessionController {
+
+	@RequestMapping(value = "/saveSession", method = RequestMethod.GET)
+	public @ResponseBody
+	BodyToken getUrlVideo(HttpServletRequest request,
+			HttpServletResponse response,
+			@RequestParam(value="idToken", required = false) String token) throws Exception {
+		BodyToken bodyToken = DecodeUtil.decodeToken(token);
+		Sessions.nuevo(bodyToken);
+		return bodyToken;
+	}
+}
